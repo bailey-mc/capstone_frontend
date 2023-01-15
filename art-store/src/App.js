0f6742cb817e448bd11fd989424ea3406235de72
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-01-15 16:07:52 -0600

    react router installed, root route set up (index stuff moved to root)

--- art-store/src/App.js
+++ art-store/src/App.js
@@ -1,42 +1,67 @@
 import axios from 'axios'
 import React, {useEffect, useState} from 'react'
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes
+// } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './App.css';
+import Root from './routes/Root'
+import ErrorPage from './routes/Error-page';
 
 function App() {
-  const [art, setArt] = useState([{
-                                "artName": "",
-                                "artist": "",
-                                "id": "",
-                                "price": "",
-                                "year": ""
-  }])
 
-  const getArt = () => {
-    axios
-      .get('http://localhost:5245/api/art')
-      .then 
-      (response => setArt(response.data))
-      console.log(art );
 
-  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
 
-  useEffect(()=> {
-    getArt()
-  }, [])
-  return (
-    <div className="App">
-      {art.map((artPiece) => {
-          return(
-            <div key={artPiece.id}>
-              {artPiece.artName} by
-              {artPiece.artist}
-            </div>
-          )
-        })
-      }
-      hewwo
-    </div>
+    },
+  ]);
+
+  
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
+
+
+  // return (
+
+  //   <>
+  //   <Router>
+  //     <Routes>
+  //       <Route path="/" index 
+  //       element={<Index getArt={getArt} art={art} setArt={setArt}/>}
+  //       errorElement= {<ErrorPage/>}
+  //       />
+
+  //     </Routes>
+    
+  //   </Router>
+  //   </>
+
+
+    // <div className="App">
+    //   {art.map((artPiece) => {
+    //       return(
+    //         <div key={artPiece.id}>
+    //           <p>{artPiece.artName} by {artPiece.artist}</p>
+    //         </div>
+    //       )
+    //     })
+    //   }
+    // </div>
+  // );
 }
 
 export default App;
